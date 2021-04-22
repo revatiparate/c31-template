45a8774334c12fb71dd9e8d5b3a2ac50140b8dee
revatiparate <74237513+revatiparate@users.noreply.github.com>
2021-04-22 12:55:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+
+  
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+  ground.display();
+  
+   /*for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }*/
+}
